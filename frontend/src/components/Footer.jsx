5c9c56ec4fdf05fe49835046ad79cb5da2e96d02
sebastien b <bas.envoi2604@gmail.com>
2023-11-08 21:12:51 +0100

    footer et nav sans css

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,22 @@
+function Footer() {
+  return (
+    <div>
+      <p>Obscuria© 2023 by Theo, Younes, Seb, Greg</p>
+
+      <a href="https://www.facebook.com">
+        <i className="fa-brands fa-facebook" />
+      </a>
+      <a href="https://www.tiktok.com">
+        <i className="fa-brands fa-tiktok" />
+      </a>
+
+      <a href="https://www.github.com">
+        <i className="fa-brands fa-github" />
+      </a>
+
+      <a href="/about">About</a>
+    </div>
+  );
+}
+
+export default Footer;
---
+++ frontend/src/components/Title.jsx
@@ -0,0 +1,10 @@
+function Title() {
+  return (
+    <div>
+      <h1>OBSCURIA</h1>
+      <h4>A L'AFFICHE</h4>
+    </div>
+  );
+}
+
+export default Title;
