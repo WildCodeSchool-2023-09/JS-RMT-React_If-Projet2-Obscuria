2086bcc2a2f8ec4d1977b69a48a00c82aa6a60e4
sebastien b <bas.envoi2604@gmail.com>
2023-11-10 13:41:03 +0100

    importation film base de données et mise en place css et media screen

--- frontend/src/components/Film.jsx
+++ frontend/src/components/Film.jsx
@@ -4,20 +4,18 @@ function Affiche() {
   const films = useLoaderData();
 
   return (
-    <div>
-      <div className="contener">
-        {films.map((film) => {
-          return (
-            <div className="carte" key={film.id}>
-              <img
-                className="borderimg"
-                src={film.poster_path}
-                alt={film.original_title}
-              />
-            </div>
-          );
-        })}
-      </div>
+    <div className="contener">
+      {films.map((film) => {
+        return (
+          <div className="carte" key={film.id}>
+            <img
+              className="borderimg"
+              src={film.poster_path}
+              alt={film.original_title}
+            />
+          </div>
+        );
+      })}
     </div>
   );
 }
