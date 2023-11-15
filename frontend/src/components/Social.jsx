ecd2303cb2f11b0e5e2bbdf96080599df1de9321
Grégory Duffort <g.duffort@gmail.com>
2023-11-15 09:38:42 +0100

    Modifications orthographe, css et création d'un composant pour les liens vers les reseaux sociaux

---
+++ frontend/src/components/Social.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+function Social() {
+  return (
+    <div className="container-low">
+      <h3 className="title-website-footer">OBSCURIA</h3>
+      <div className="logo-contact">
+        <div className="logo">
+          <a href="https://www.facebook.com" target="_blank" rel="noreferrer">
+            <i className="fab fa-facebook" />
+          </a>
+          <a href="https://www.tiktok.com" target="_blank" rel="noreferrer">
+            <i className="fab fa-tiktok" />
+          </a>
+          <a href="https://www.github.com" target="_blank" rel="noreferrer">
+            <i className="fab fa-github" />
+          </a>
+        </div>
+        <div className="about">
+          <h4>ABOUT</h4>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Social;
