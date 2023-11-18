f1deb33c5a98b766d5494f49a6febea3cd62bcc4
jean519 <129189905+jean519@users.noreply.github.com>
2023-11-18 14:58:19 +0100

    changement de style et des card du loader

---
+++ frontend/src/components/DetailMovie.jsx
@@ -0,0 +1,54 @@
+import { Link, useLoaderData } from "react-router-dom";
+import "./MovieCard.css";
+import "./DetailMovie.css";
+
+function DetailMovie() {
+  const movie = useLoaderData();
+  return (
+    <div className="bigContainer">
+      <Link className="link" to="/">
+        <figure className="cardDetailContainer">
+          <div className="cartDetailTitel">
+            <div className="cardDetailTitlelft">
+              <p className="cardDetail_h1">{movie.original_title}</p>
+              <p>{movie.genre_ids}</p>
+            </div>
+          </div>
+          <div className="containerHour">
+            <div className="cardDetailcontainerimg">
+              <img
+                className="cardDetailImg"
+                src={movie.poster_path}
+                alt={movie.original_title}
+              />
+            </div>
+
+            <div className="containerHourRight">
+              <div className="cardDetailDayContainer">
+                <span className="cardDetailDay">jeudi 16 novembre</span>
+              </div>
+              <div className="firsthour">
+                <div className="cardDetailhour">10h </div>
+                <div className="cardDetailhour">14h </div>
+              </div>
+              <div className="secondHour">
+                <div className="cardDetailhour">16h </div>
+                <div className="cardDetailhour">19h </div>
+              </div>
+              <div className="thirdHour">
+                <div className="cardDetailhour">21h30 </div>
+              </div>
+              <div className="cardDetailAbout">
+                <p className="CardDetailLanguage">{movie.original_language}</p>
+                <span className="cardDetailvote">{movie.vote_average}</span>
+              </div>
+            </div>
+          </div>
+          <figcaption className="cardDetailover">{movie.overview}</figcaption>
+        </figure>
+      </Link>
+    </div>
+  );
+}
+
+export default DetailMovie;
---
+++ frontend/src/pages/PageMovie.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import DetailMovie from "../components/DetailMovie";
+
+function PageMovie() {
+  return (
+    <div className="Page">
+      <DetailMovie />
+    </div>
+  );
+}
+
+export default PageMovie;
