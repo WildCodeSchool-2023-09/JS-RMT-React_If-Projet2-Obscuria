8c60894242ba36b897cde878c8d63118de26322b
jean519 <129189905+jean519@users.noreply.github.com>
2023-11-18 13:11:19 +0100

    changement de style du carousselle et carte

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,6 @@ import DisplayMovie from "./components/DisplayMovie";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import FormContact from "./components/FormContact";
-import Button from "./components/Button";
 import "./App.css";
 
 function App() {
--- frontend/src/components/carouselMenu.jsx
+++ frontend/src/components/carouselMenu.jsx
@@ -30,7 +30,7 @@ function CarouselMenu() {
   };
 
   return (
-    <div>
+    <div className="slider-container">
       <Slider
         dots={carouselSettings.dots}
         infinite={carouselSettings.infinite}
@@ -49,8 +49,8 @@ function CarouselMenu() {
               alt={film.original_title}
             />
             <div className="text-right-slide">
-              <p>{film.original_title}</p>
-              <p>{film.overview}</p>
+              <p className="film-original-title">{film.original_title}</p>
+              <p className="film-description">{film.overview}</p>
               <button type="button" className="button-ticket">
                 Réservez votre billet !
               </button>
